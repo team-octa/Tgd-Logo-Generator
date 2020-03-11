6cb3c89ae0642c45e928ae3e66c304b76fb8f417
cheongan <smartnation@naver.com>
2020-03-11 09:30:03 +0900

    feat: ⚡️ Add Text to Image Generater.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,30 +1,44 @@
 import { Router, Request, Response } from 'express'
 
+import { ti } from './text-to-image'
 
-import { registerFont } from 'canvas';
-
-import ti from "text-to-image";
-
-import path from "path"
+import path from 'path'
 
 const router = Router()
 
 router.get('/', (req: Request, res: Response) => {
-    registerFont(path.join("src/public", "font", "TmonMonsori.ttf"), { family: "TmonMonsori" })
+  ti.generate('트게더', {
+    debug: true,
+    maxWidth: 150,
+    fontFamily: 'TmonMonsori',
+    fontSize: 48,
+    lineHeight: 50,
+    bgColor: 'transparent',
+    textColor: '#6b4aa9',
+    textAlign: 'center',
+  }).then(function(dataUri) {
+    res.render('index', { src: dataUri, fontSize: 48 })
+  })
+})
 
-    ti.generate('웨 안돼 ㅠㅠ', {
-        debug: true,
-        maxWidth: 720,
-        fontFamily: "TmonMonsori",
-        lineHeight: 30,
-        margin: 5,
-        bgColor: "white",
-        textColor: "red"
-    }).then(function (dataUri) {
-        res.render('index', { src: dataUri })
-    });
+router.post('/', (req: Request, res: Response) => {
+  const { font = 46 } = req.body
+
+  ti.generate('청게더', {
+    debug: true,
+    maxWidth: 140,
+    fontFamily: 'TmonMonsori',
+    fontSize: font,
+    lineHeight: 50,
+    margin: 5,
+    bgColor: 'transparent',
+    textColor: '#8A2BE2',
+    textAlign: 'center',
+  }).then(dataUri => {
+    res.render('index', { src: dataUri, fontSize: font })
+  })
 })
 
 export const IndexRouter = {
-    router,
-};
+  router,
+}
